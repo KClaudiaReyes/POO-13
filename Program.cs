3107e4727d05ea4d6d0ab73a0ca0b2171ad330cc
clauR <creyesccf@gmail.com>
2026-03-12 20:16:04 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class DispositivosTec
+{
+    public string Nombre;
+    public int resultadoPrueba1;
+    public int resultadoPrueba2;
+    public int resultadoPrueba3;
+
+    public double CalcularPromedio()
+    {
+        return (resultadoPrueba1 + resultadoPrueba2 + resultadoPrueba3) / 3;
+    }
+
+    public string ObtenerEstado()
+    {
+        if (CalcularPromedio() >= 80) return "Optimo";
+        else return 
+    }
+}
